86cba0fc7a0abc4a75b2cbba86e57b4dc8d8bb3f
Jbohacek <jbohacek@email.cz>
2024-03-10 18:08:55 +0100

    Added GOF

---
+++ Maturita.Web/Areas/Programovani/Controllers/GofController.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Maturita.Web.Areas.Programovani.Controllers
+{
+    [Area("Programovani")]
+    public class GofController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Factory()
+        {
+            return View();
+        }
+
+        public IActionResult Singleton()
+        {
+            return View();
+        }
+
+        public IActionResult Command()
+        {
+            return View();
+        }
+
+        public IActionResult Observer()
+        {
+            return View();
+        }
+
+        public IActionResult Iterator()
+        {
+            return View();
+        }
+
+        public IActionResult State()
+        {
+            return View();
+        }
+
+        public IActionResult Decorator()
+        {
+            return View();
+        }
+
+        public IActionResult Memento()
+        {
+            return View();
+        }
+
+        public IActionResult Adapter()
+        {
+            return View();
+        }
+    }
+}
