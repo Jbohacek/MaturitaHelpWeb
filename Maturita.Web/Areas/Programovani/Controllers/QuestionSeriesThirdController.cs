fc96c0fa65a8d4a0253a74a3e1b9db2545e348be
Jbohacek <jbohacek@email.cz>
2024-03-25 19:58:56 +0100

    Database

--- Maturita.Web/Areas/Programovani/Controllers/QuestionSeriesThirdController.cs
+++ Maturita.Web/Areas/Programovani/Controllers/QuestionSeriesThirdController.cs
@@ -2,8 +2,10 @@
 
 namespace Maturita.Web.Areas.Programovani.Controllers
 {
+    [Area("Programovani")]
     public class QuestionSeriesThirdController : Controller
     {
+        
         public IActionResult Databaze()
         {
             return View();
