f8d55e17ae2c30ddc76a0fc4e5849ba3623f900c
Jbohacek <jbohacek@email.cz>
2024-03-25 18:19:15 +0100

    škola

---
+++ Maturita.Web/Areas/Programovani/Controllers/QuestionSeriesThirdController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Maturita.Web.Areas.Programovani.Controllers
+{
+    public class QuestionSeriesThirdController : Controller
+    {
+        public IActionResult Databaze()
+        {
+            return View();
+        }
+    }
+}
