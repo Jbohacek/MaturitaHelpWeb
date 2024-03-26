0ee395734d3827b3ea8bd0ac601106ff5ca88b41
Jbohacek <jbohacek@email.cz>
2024-03-26 19:58:47 +0100

    Unit test

--- Maturita.Web/Areas/Programovani/Controllers/QuestionSeriesThirdController.cs
+++ Maturita.Web/Areas/Programovani/Controllers/QuestionSeriesThirdController.cs
@@ -5,8 +5,20 @@ namespace Maturita.Web.Areas.Programovani.Controllers
     [Area("Programovani")]
     public class QuestionSeriesThirdController : Controller
     {
-        
+        //21
         public IActionResult Databaze()
+        {
+            return View();
+        }
+
+
+        //22
+        public IActionResult TeorieTestovani()
+        {
+            return Json("pokus");
+        }
+
+        public IActionResult UnitTesty()
         {
             return View();
         }
