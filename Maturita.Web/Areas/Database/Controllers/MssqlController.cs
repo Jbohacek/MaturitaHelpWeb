6f81ae61219e6046b28e7ebc5aada8b712cee68f
Jbohacek <jbohacek@email.cz>
2024-04-07 21:34:54 +0200

    Mirroring, replication, témata přidaná

---
+++ Maturita.Web/Areas/Database/Controllers/MssqlController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Maturita.Web.Areas.Database.Controllers
+{
+    [Area("Database")]
+    public class MssqlController : Controller
+    {
+        public IActionResult Replication()
+        {
+            return View();
+        }
+
+        public IActionResult Mirroring()
+        {
+            return View();
+        }
+    }
+}
