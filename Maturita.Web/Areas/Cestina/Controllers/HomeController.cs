c52d168d198a118e60560ff5830f721b4c54b915
Jbohacek <jbohacek@email.cz>
2024-04-11 21:44:55 +0200

    Added Čeština

---
+++ Maturita.Web/Areas/Cestina/Controllers/HomeController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Maturita.Web.Areas.Cestina.Controllers
+{
+    [Area("Cestina")]
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
