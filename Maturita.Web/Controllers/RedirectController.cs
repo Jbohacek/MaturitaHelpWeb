e2e92b2fa21f380734f598db82f00550cc4ce883
bohacekjakub <bohacekjakub@sssvt.cz>
2024-04-11 09:32:49 +0200

    Přesměrování

---
+++ Maturita.Web/Controllers/RedirectController.cs
@@ -0,0 +1,20 @@
+﻿using Maturita.Web.Models.Functional;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Maturita.Web.Controllers
+{
+    public class RedirectController : Controller
+    {
+        public IActionResult Index(string url, string desc, string backUrl)
+        {
+            RedirectModel redirectModel = new RedirectModel()
+            {
+                Url = url,
+                Description = desc,
+                BackUrl = backUrl
+            };
+
+            return View("_RedirectSite", redirectModel);
+        }
+    }
+}
---
+++ Maturita.Web/Models/Functional/RedirectModel.cs
@@ -0,0 +1,12 @@
+﻿namespace Maturita.Web.Models.Functional
+{
+    public class RedirectModel
+    {
+        public string Url { get; set; }
+
+        public string Description { get; set; }
+
+        public string BackUrl { get; set; }
+
+    }
+}
