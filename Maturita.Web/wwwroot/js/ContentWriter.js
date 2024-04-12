0f96073ffbca532a946d38be68472a0125f98dc8
Jbohacek <jbohacek@email.cz>
2024-04-12 16:42:20 +0200

    Scroll up, content creator added

---
+++ Maturita.Web/wwwroot/js/ContentWriter.js
@@ -0,0 +1,30 @@
+﻿const headers = document.querySelectorAll('.display-2');
+
+const mainHeader = document.querySelector('.display-1');
+
+const mainContent = document.createElement('div');
+const listHeaders = document.createElement('ul');
+
+mainHeader.classList.add('mb-0');
+listHeaders.classList.add('content-header-list');
+
+
+headers.forEach(x => {
+    x.id = x.innerText;
+    var liElement = document.createElement('li');
+    var aElement = document.createElement('a');
+
+    aElement.href = '#' + x.innerText;
+    aElement.innerText = x.innerText;
+
+    liElement.appendChild(aElement);
+    listHeaders.appendChild(liElement);
+
+    }
+);
+
+mainContent.appendChild(listHeaders);
+
+mainHeader.insertAdjacentElement('afterend', mainContent);
+
+
--- Maturita.Web/wwwroot/js/ScrollAnimate.js
+++ Maturita.Web/wwwroot/js/ScrollAnimate.js
@@ -15,3 +15,15 @@ hiddenElements.forEach((el) => observer.observe(el));
 
 const hiddenElements2 = document.querySelectorAll('.hidden');
 hiddenElements2.forEach((el) => observer.observe(el));
+
+
+window.addEventListener('scroll', function () {
+    const scrollPosition = window.scrollY;
+    const triggerHeight = 300; 
+
+    if (scrollPosition >= triggerHeight) {
+        document.getElementById('GoUp').classList.remove('go-up-notActive');
+    } else {
+        document.getElementById('GoUp').classList.add('go-up-notActive');
+    }
+});
