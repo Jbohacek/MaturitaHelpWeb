a5a47441df015f85caea83220b1c5b634c767320
Jbohacek <jbohacek@email.cz>
2024-04-12 17:36:05 +0200

    Added access stuff

---
+++ Maturita.Web/Areas/ProgramoveVybaveni/Controllers/AccessController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Maturita.Web.Areas.ProgramoveVybaveni.Controllers
+{
+    [Area("ProgramoveVybaveni")]
+    public class AccessController : Controller
+    {
+        public IActionResult TabulkyARelace()
+        {
+            return View();
+        }
+
+        public IActionResult Dotazy()
+        {
+            return View();
+        }
+
+        public IActionResult FunkceVDotazech()
+        {
+            return View();
+        }
+
+        public IActionResult Formulare()
+        {
+            return View();
+        }
+
+        public IActionResult Sestavy()
+        {
+            return View();
+        }
+
+        public IActionResult ImportExport()
+        {
+            return View();
+        }
+    }
+}
--- Maturita.Web/wwwroot/js/site.js
+++ Maturita.Web/wwwroot/js/site.js
@@ -5,3 +5,7 @@ setTimeout(RevealSpacer, 10);
 function RevealSpacer() {
     spacer.classList.remove('hidden-spacer');
 }
+
+const disabledLinks = document.querySelectorAll('[Disabled]');
+
+disabledLinks.forEach(x => x.removeAttribute('href'));
