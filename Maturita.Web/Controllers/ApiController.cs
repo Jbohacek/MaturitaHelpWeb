ecacceb3e81bf07627a6aa8d597640e3466154ca
Jbohacek <jbohacek@email.cz>
2024-04-26 22:55:54 +0200

    ApiAdd

---
+++ Maturita.Web/Controllers/ApiController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+
+namespace Maturita.Web.Controllers
+{
+    [Route("api/[action]")]
+    [ApiController]
+    public class ApiController : ControllerBase
+    {
+        public IActionResult Data()
+        {
+            return new JsonResult(
+                new { IsSucces = "Ahoj, tohle je testovaci api" }
+                );
+        }
+    }
+}
